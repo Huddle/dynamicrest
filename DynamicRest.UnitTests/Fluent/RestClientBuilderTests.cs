2ddb90cd4a0723934250220e6cb54abaaa5dd6b2
Francesco Pighi <francesco.pighi@gmail.com>
2011-03-07 15:33:45 +0000

    Added accept header to fluent RestClient builder

--- DynamicRest.UnitTests/Fluent/RestClientBuilderTests.cs
+++ DynamicRest.UnitTests/Fluent/RestClientBuilderTests.cs
@@ -5,6 +5,11 @@ using Machine.Specifications;
 namespace DynamicRest.UnitTests.Fluent {
     public class When_a_rest_client_is_created_with_a_fluent_factory {
 
+        static IRestClientBuilder _restClientBuilder;
+        static dynamic _builtClient;
+
+        static FakeHttpRequestFactory fakeHttpRequestFactory;
+
         Establish context = () => {
             _restClientBuilder = new RestClientBuilder();
         };
@@ -13,25 +18,26 @@ namespace DynamicRest.UnitTests.Fluent {
             fakeHttpRequestFactory = new FakeHttpRequestFactory();
             _builtClient = _restClientBuilder
                 .WithRequestBuilder(new HttpVerbRequestBuilder(fakeHttpRequestFactory))
-                .WithContentType("content/type")
+                .WithContentType("application/vnd.data+xml")
                 .WithUri("http://www.google.com")
+                .WithBody("My body")
+                .WithAcceptHeader("application/xml") 
                 .Build();
 
             _builtClient.Post();
         };
 
-        It should_not_be_null = () => ShouldExtensionMethods.ShouldNotBeNull(_builtClient);
+        It should_have_the_correct_content_type_header = () =>
+            (fakeHttpRequestFactory.CreatedRequest as FakeHttpWebRequestWrapper).GetContentType().ShouldEqual("application/vnd.data+xml");
 
-        private It should_have_the_correct_content_type_header = () => 
-            (fakeHttpRequestFactory.CreatedRequest as FakeHttpWebRequestWrapper).GetContentType().ShouldEqual("content/type");
+        It should_have_the_correct_uri = () => 
+            fakeHttpRequestFactory.CreatedRequest.RequestURI.ShouldEqual(new Uri("http://www.google.com"));
 
-        private It should_have_the_correct_uri =
-            () => fakeHttpRequestFactory.CreatedRequest.RequestURI.ShouldEqual(new Uri("http://www.google.com"));
+        It should_have_the_correct_body = () =>
+            (fakeHttpRequestFactory.CreatedRequest as FakeHttpWebRequestWrapper).GetRequestBody().ShouldEqual("My body");
 
-        private static IRestClientBuilder _restClientBuilder;
-        private static dynamic _builtClient;
-
-        private static FakeHttpRequestFactory fakeHttpRequestFactory;
+        It should_have_the_correct_accept_header =
+            () => fakeHttpRequestFactory.CreatedRequest.Accept.ShouldEqual("application/xml");
     }
 
     public interface IRestClientBuilder {
@@ -40,16 +46,22 @@ namespace DynamicRest.UnitTests.Fluent {
         IRestClientBuilder WithContentType(string contentType);
         IRestClientBuilder WithRequestBuilder(IBuildRequests requestBuilder);
         IRestClientBuilder WithUri(string uri);
+        IRestClientBuilder WithBody(string body);
+        IRestClientBuilder WithAcceptHeader(string acceptType);
     }
 
     public class RestClientBuilder : IRestClientBuilder {
-        private IBuildRequests _requestBuilder;
-        private string _uri;
-        private string _contentType;
+        IBuildRequests _requestBuilder;
+        string _uri;
+        string _contentType;
+        string _body;
+        string _acceptType;
 
         public dynamic Build() {
             _requestBuilder.Uri = _uri;
             _requestBuilder.ContentType = _contentType;
+            _requestBuilder.Body = _body;
+            _requestBuilder.AcceptHeader = _acceptType;
             return new RestClient(_requestBuilder, new ResponseProcessor(RestService.Xml));
         }
 
@@ -66,6 +78,17 @@ namespace DynamicRest.UnitTests.Fluent {
         public IRestClientBuilder WithUri(string uri) {
             _uri = uri;
             return this;
+        }
+
+        public IRestClientBuilder WithBody(string body) {
+            _body = body;
+            return this;
+        }
+
+        public IRestClientBuilder WithAcceptHeader(string acceptType) {
+            _acceptType = acceptType;
+            return this;
+
         }
     }
 }
