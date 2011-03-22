9bde1ae454453868029f7ccfc29944b122b20133
unknown <bob.gregory@.huddle.local>
2011-03-22 16:41:06 +0000

    Added implicit conversion operators for DateTime, int, bool, and long to XmlString

--- DynamicRest.UnitTests/Xml/XmlResponsesTests.cs
+++ DynamicRest.UnitTests/Xml/XmlResponsesTests.cs
@@ -25,6 +25,9 @@ namespace DynamicRest.UnitTests.Xml
         It should_contain_the_attachment_title = () => (_response.item.attachments.attachment[0].title.Value as string).ShouldEqual("this is the title");
         It should_contain_the_numbers_of_attachments = () => ((int)_response.item.attachments.Count).ShouldEqual(1);
 
+        It should_contain_the_pubdate =
+            () => ((DateTime)_response.item.pubDate).ShouldEqual(new DateTime(2011, 3, 8, 11, 21, 16));
+
         It should_work_when_a_refence_is_used_as_an_array = () =>
         {
             var linkAsArray = _response.item.link;
@@ -34,7 +37,8 @@ namespace DynamicRest.UnitTests.Xml
         static string _xml = @"
             <news>
               <item> 
-                <title>Gaddafi renews attack on rebels</title>  
+                <title>Gaddafi renews attack on rebels</title>
+                <version>5</version>  
                 <description>Forces loyal to Libyan leader Col Muammar Gaddafi launch fresh air strikes on the rebel-held Ras Lanuf, as they try to retake the oil-rich town.</description>
                 <link>http://www.bbc.co.uk/go/rss/int/news/-/news/world-africa-12673956</link>  
                 <link>http://www.bbc.co.uk/go/rss/int/news/-/news/world-africa-12673956</link>  
@@ -96,5 +100,194 @@ namespace DynamicRest.UnitTests.Xml
             </news>";
 
         private static Exception _thrownException;
+    }
+
+
+    namespace When_casting_strings_to_datetimes
+    {
+        [Subject(typeof(XmlString))]
+        public class When_a_string_is_a_valid_date
+        {
+            Establish context = () => The_string = new XmlString("2011-03-08T11:21:16Z");
+
+            Because we_parse_as_a_date = () => The_result = (DateTime)The_string;
+
+            It should_have_the_expected_value = () => The_result.ShouldEqual(new DateTime(2011, 3, 8, 11, 21, 16));
+
+            private static XmlString The_string;
+            private static DateTime The_result;
+        }
+
+        [Subject(typeof(XmlString))]
+        public class When_parsing_fails
+        {
+            Establish context = () => The_string = new XmlString("I am not a valid string");
+
+            Because we_parse_as_a_date = () => The_result = Catch.Exception(() => Console.Write((DateTime)The_string));
+
+            private It should_have_the_expected_value = () => The_result.ShouldBeOfType<FormatException>();
+
+            private static XmlString The_string;
+            private static Exception The_result;
+        }
+    }
+
+    namespace When_casting_strings_to_integers
+    {
+        [Subject(typeof(XmlString))]
+        public class When_a_string_is_a_valid_int
+        {
+            Establish context = () => The_string = new XmlString("8787645");
+
+            Because we_parse_as_an_int = () => The_result = (int)The_string;
+
+            It should_have_the_expected_value = () => The_result.ShouldEqual(8787645);
+
+            private static XmlString The_string;
+            private static int The_result;
+        }
+
+        [Subject(typeof(XmlString))]
+        public class When_a_string_is_an_overflow
+        {
+            Establish context = () => The_string = new XmlString( (((long)int.MaxValue) + 1).ToString());
+
+            Because we_parse_as_an_int = () => The_result = Catch.Exception(() => Console.Write((int)The_string));
+
+            private It should_have_the_expected_value = () => The_result.ShouldBeOfType<OverflowException>();
+
+            private static XmlString The_string;
+            private static Exception The_result;
+        }
+
+
+        [Subject(typeof(XmlString))]
+        public class When_parsing_fails
+        {
+            Establish context = () => The_string = new XmlString("I am not a valid string");
+
+            Because we_parse_as_an_int= () => The_result = Catch.Exception(() => Console.Write((int)The_string));
+
+            private It should_have_the_expected_value = () => The_result.ShouldBeOfType<FormatException>();
+
+            private static XmlString The_string;
+            private static Exception The_result;
+        }
+    }
+
+    namespace When_casting_strings_to_longs
+    {
+        [Subject(typeof(XmlString))]
+        public class When_a_string_is_a_valid_long
+        {
+            Establish context = () => The_string = new XmlString("8787645");
+
+            Because we_parse_as_a_long = () => The_result = (long)The_string;
+
+            It should_have_the_expected_value = () => The_result.ShouldEqual(8787645);
+
+            private static XmlString The_string;
+            private static long The_result;
+        }
+
+        [Subject(typeof(XmlString))]
+        public class When_a_string_is_an_overflow
+        {
+            Establish context = () => The_string = new XmlString((long.MaxValue)+"1");
+
+            Because we_parse_as_a_long = () => The_result = Catch.Exception(() => Console.Write((long)The_string));
+
+            private It should_have_the_expected_value = () => The_result.ShouldBeOfType<OverflowException>();
+
+            private static XmlString The_string;
+            private static Exception The_result;
+        }
+
+
+        [Subject(typeof(XmlString))]
+        public class When_parsing_fails
+        {
+            Establish context = () => The_string = new XmlString("I am not a valid string");
+
+            Because we_parse_as_a_long = () => The_result = Catch.Exception(() => Console.Write((long)The_string));
+
+            private It should_have_the_expected_value = () => The_result.ShouldBeOfType<FormatException>();
+
+            private static XmlString The_string;
+            private static Exception The_result;
+        }
+    }
+ 
+    namespace When_casting_strings_to_bools
+    {
+        public class When_parsing_the_empty_string
+        {
+            Because we_parse_string_empty =
+                () => Result = Catch.Exception(() => Console.Write((bool) new XmlString(string.Empty)));
+
+            It should_throw_format_exception = () => Result.ShouldBeOfType<FormatException>();
+
+            protected static Exception Result { get; set; }
+        }
+
+        public class When_parsing_the_zero_string
+        {
+            Because we_parse_string_empty =
+                () => Result = (bool) new XmlString("0");
+
+            It should_be_false = () => Result.ShouldBeFalse();
+
+            protected static bool Result { get; set; }
+        }
+
+        public class When_parsing_negative_one
+        {
+            Because we_parse_string_empty =
+                () => Result = (bool)new XmlString("-1");
+
+            It should_be_false = () => Result.ShouldBeFalse();
+
+            protected static bool Result { get; set; }
+        }
+
+        public class When_parsing_one
+        {
+            Because we_parse_string_empty =
+                () => Result = (bool)new XmlString("1");
+
+            It should_be_false = () => Result.ShouldBeTrue();
+
+            protected static bool Result { get; set; }
+        }
+
+        public class When_parsing_true
+        {
+            Because we_parse_string_empty =
+                () => Result = (bool)new XmlString("true");
+
+            It should_be_false = () => Result.ShouldBeTrue();
+
+            protected static bool Result { get; set; }
+        }
+
+        public class When_parsing_false
+        {
+            Because we_parse_string_empty =
+                () => Result = (bool)new XmlString("false");
+
+            It should_be_false = () => Result.ShouldBeFalse();
+
+            protected static bool Result { get; set; }
+        }
+
+        public class When_parsing_weird_casing
+        {
+            Because we_parse_string_empty =
+                () => Result = (bool)new XmlString("tRUe");
+
+            It should_be_false = () => Result.ShouldBeTrue();
+
+            protected static bool Result { get; set; }
+        }
     }
 }
