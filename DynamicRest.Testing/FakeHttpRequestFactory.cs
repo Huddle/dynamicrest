c46ed349db614ecd4d08e19b8ec41a487bd1c211
Bob Gregory <bob@huddle.com>
2011-09-07 15:43:14 +0100

    Moving test doubles to a separate assembly so they can be used by third parties

---
+++ DynamicRest.Testing/FakeHttpRequestFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DynamicRest.HTTPInterfaces;
+
+namespace DynamicRest.Testing {
+    public class FakeHttpRequestFactory : IHttpRequestFactory {
+        private IHttpResponse _httpResponse;
+
+        public FakeHttpWebRequestWrapper CreatedRequest {get; set; }
+
+        public IHttpRequest Create(Uri uri) {
+            CreatedRequest = new FakeHttpWebRequestWrapper(uri);
+            if (_httpResponse != null)
+            {
+                CreatedRequest.Response = _httpResponse;
+            }
+            return CreatedRequest;
+        }
+
+        public void SetResponse(IHttpResponse httpResponse)
+        {
+             _httpResponse = httpResponse;
+        }
+    }
+}
