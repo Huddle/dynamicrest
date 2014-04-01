aa59e776af7df750623a029d221ba98519b9cc0a
Ian Pender <ian.pender@huddle.com>
2014-04-01 09:30:20 +0100

    Added test for Delete verb

--- DynamicRest.UnitTests/RestClients/HttpVerbRequestBuilderTests.cs
+++ DynamicRest.UnitTests/RestClients/HttpVerbRequestBuilderTests.cs
@@ -146,4 +146,29 @@ namespace DynamicRest.UnitTests.RestClients {
 
         It should_default_the_auto_follow_option_to_false_on_the_request = () => requestFactory.CreatedRequest.AllowAutoRedirect.ShouldEqual(false);
     }
+
+    [Subject(typeof(HttpVerbRequestBuilder))]
+    public class When_i_use_the_client_to_delete_a_request
+    {
+        private const string TestUri = "http://api.huddle.local/v2/tasks/123456";
+        private static dynamic client;
+        private static FakeHttpRequestFactory requestFactory;
+        private static string oAuth2Token = "{my_token}";
+
+        Establish context = () =>
+        {
+            requestFactory = new FakeHttpRequestFactory();
+
+            var httpVerbRequestBuilder = new HttpVerbRequestBuilder(requestFactory) { Uri = TestUri };
+            httpVerbRequestBuilder.SetOAuth2AuthorizationHeader(oAuth2Token);
+            client = new RestClient(httpVerbRequestBuilder, new ResponseProcessor(new StandardResultBuilder(RestService.Xml)));
+        };
+
+        Because we_make_get_call_to_an_api_via_rest_client = () => client.Delete();
+
+        It should_build_the_expected_uri = () => TestUri.ShouldEqual(requestFactory.CreatedRequest.RequestURI.ToString());
+        It should_set_the_correct_http_verb_on_the_request = () => requestFactory.CreatedRequest.HttpVerb.ShouldEqual(HttpVerb.Delete);
+        It should_set_the_correct_authorization_header_on_the_request = () => requestFactory.CreatedRequest.Headers[HttpRequestHeader.Authorization].ShouldEqual(string.Format("OAuth2 {0}", oAuth2Token));
+    }
+
 }
